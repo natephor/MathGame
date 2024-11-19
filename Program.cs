afd81f98359f056a294165940f5675b9983b4bb7
Natephor <natephor@outlook.com>
2024-11-19 12:50:00 +0200

    feat: one game round

--- Program.cs
+++ Program.cs
@@ -1,3 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Welcome to Math Game!");
 
-Console.WriteLine("Hello, World!");
+string? choice;
+int selection;
+do
+{
+    DisplayMenu();
+    choice = Console.ReadLine();
+} while (!int.TryParse(choice, out selection) || selection is > 4 or < 1);
+
+var random = new Random();
+var firstNumber = random.Next(0, 101);
+var secondNumber = random.Next(0, 101);
+int result = 0;
+string output = "";
+var operation = (Operation)selection;
+switch (operation)
+{
+    case Operation.Addition:
+        result = firstNumber + secondNumber;
+        output = $"{firstNumber} + {secondNumber} = ";
+        break;
+    case Operation.Subtraction:
+        result = secondNumber - firstNumber;
+        output = $"{secondNumber} - {firstNumber} = ";
+        break;
+    case Operation.Multiplication:
+        result = firstNumber * secondNumber;
+        output = $"{firstNumber} * {secondNumber} = ";
+        break;
+    case Operation.Division:
+        secondNumber = random.Next(1, 101);
+        while (firstNumber % secondNumber != 0)
+        {
+            firstNumber = random.Next(0, 101);
+            secondNumber = random.Next(1, 101);
+        }
+
+        result = firstNumber / secondNumber;
+        output = $"{firstNumber} / {secondNumber} = ";
+        break;
+}
+
+int answer;
+string? userInput;
+do
+{
+    Console.Write(output);
+    userInput = Console.ReadLine();
+} while (!int.TryParse(userInput, out answer));
+
+Console.WriteLine(answer == result ? "You are correct!" : "You are wrong!");
+Console.WriteLine($"The correct answer of {output} was {result}.");
+Console.WriteLine("Thank you for playing!");
+
+// Randomize operation
+// var random = new Random();
+// var operation = (Operations) random.Next(1, 5);
+// Console.WriteLine(operation);
+       
+// Decide on operands
+// DisplayProblem();
+// Get answer
+// Verify answer
+
+
+void DisplayMenu()
+{
+    Console.WriteLine("Select an operation (1-4):");
+    Console.WriteLine("1. Addition");
+    Console.WriteLine("2. Subtraction");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+}
+
+enum Operation
+{
+    Addition = 1,
+    Subtraction,
+    Multiplication,
+    Division
+}
--- README.md
+++ README.md
@@ -4,11 +4,11 @@ A console app I made while attending [C# Academy](https://www.thecsharpacademy.c
 
 ## Requirements
 
-- [ ] You need to create a Math game containing the 4 basic operations
-- [ ] The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.
-- [ ] Users should be presented with a menu to choose an operation
+- [x] You need to create a Math game containing the 4 basic operations
+- [x] The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.
+- [x] Users should be presented with a menu to choose an operation
 - [ ] You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
-- [ ] You don't need to record results on a database. Once the program is closed the results will be deleted.
+- [x] You don't need to record results on a database. Once the program is closed the results will be deleted.
 
 ### Extra
 - [ ] Try to implement levels of difficulty.
